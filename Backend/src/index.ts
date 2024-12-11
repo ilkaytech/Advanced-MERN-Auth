978515fdb26e7616cf7a14d5f15e94a5e454bcf9
İlkay Öztürk <118361845+ilkaytech@users.noreply.github.com>
2024-12-11 15:22:37 +0300

    feat(auth): implement register API with model and route

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -3,12 +3,14 @@ import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
 import cookieParser from "cookie-parser";
 import { config } from "./config/app.config";
-import connectDatabase from "./databae/database";
+import connectDatabase from "./database/database";
 import { errorHandler } from "./middlewares/errorHandler";
 import { HTTPSTATUS } from "./config/http.config";
+import { asyncHandler } from "./middlewares/asyncHandler";
+import authRoutes from "./modules/auth/auth.routes";
 
 const app = express();
-//const BASE_PATH = config.BASE_PATH;
+const BASE_PATH = config.BASE_PATH;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -21,16 +23,16 @@ app.use(
 
 app.use(cookieParser());
 
-app.get("/", async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    throw new Error("Error occurred while");
+app.get(
+  "/",
+  asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
     res.status(HTTPSTATUS.OK).json({
-      message: "Hello İlkay!!!",
+      message: "Hello ilkay!!!",
     });
-  } catch (error) {
-    next(error);
-  }
-});
+  })
+);
+
+app.use(`${BASE_PATH}/auth`, authRoutes);
 
 app.use(errorHandler);
 
