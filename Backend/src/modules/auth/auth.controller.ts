3ec13562166a0b1e85ce9c5f4fde548dc09f21a8
İlkay Öztürk <118361845+ilkaytech@users.noreply.github.com>
2024-12-17 15:20:05 +0300

    feat(auth): implement verify email API with model and route

--- Backend/src/modules/auth/auth.controller.ts
+++ Backend/src/modules/auth/auth.controller.ts
@@ -5,6 +5,8 @@ import { HTTPSTATUS } from "../../config/http.config";
 import {
   loginSchema,
   registerSchema,
+  verificationCodeSchema,
+  verificationEmailSchema,
 } from "../../common/validators/auth.validator";
 import {
   getAccessTokenCookieOptions,
@@ -87,6 +89,16 @@ export class AuthController {
         .json({
           message: "Refresh access token successfully",
         });
+    }
+  );
+  public verifyEmail = asyncHandler(
+    async (req: Request, res: Response): Promise<any> => {
+      const { code } = verificationEmailSchema.parse(req.body);
+      await this.authService.verifyEmail(code);
+
+      return res.status(HTTPSTATUS.OK).json({
+        message: "Email verified successfully",
+      });
     }
   );
 }
